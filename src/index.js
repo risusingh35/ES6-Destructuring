d2cd5f096aa12406ea9030ffa9284e6803658a05
Risu Singh <risusingh35@gmail.com>
2021-10-30 07:31:14 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+//import animals from "./data";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
+
+const [honda, tesla] = cars;
+
+const {
+  speedStats: { topSpeed: hondaTopSpeed }
+} = honda;
+const {
+  speedStats: { topSpeed: teslaTopSpeed }
+} = tesla;
+const {
+  coloursByPopularity: [hondaTopColour]
+} = honda;
+const {
+  coloursByPopularity: [teslaTopColour]
+} = tesla;
+
+ReactDOM.render(
+  <table>
+    <tbody>
+      <tr>
+        <th>Brand</th>
+        <th>Top Speed</th>
+        <th>Colour</th>
+      </tr>
+      <tr>
+        <td>{tesla.model}</td>
+        <td>{teslaTopSpeed}</td>
+        <td>{teslaTopColour}</td>
+      </tr>
+      <tr>
+        <td>{honda.model}</td>
+        <td>{hondaTopSpeed}</td>
+        <td>{hondaTopColour}</td>
+      </tr>
+    </tbody>
+  </table>,
+  document.getElementById("root")
+);
